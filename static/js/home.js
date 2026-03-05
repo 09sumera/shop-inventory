6ec77b17ae86e57193588806d076499af157f65c
Sumera Anjum <sumera0904@gmail.com>
2026-03-05 15:56:31 +0530

    Fix KPI stats API for deployment

--- static/js/home.js
+++ static/js/home.js
@@ -1,11 +1,11 @@
-fetch("http://127.0.0.1:5000/stats")
+fetch("/stats")
     .then(res => res.json())
     .then(data => {
         document.getElementById("kpiProducts").innerText = data.totalProducts;
         document.getElementById("kpiQuantity").innerText = data.totalQuantity;
         document.getElementById("kpiLow").innerText = data.lowStock;
-        document.getElementById("kpiValue").innerText = data.inventoryValue;
+        document.getElementById("kpiValue").innerText = "₹ " + data.inventoryValue;
     })
-    .catch(() => {
-        console.log("Failed to load KPI data");
+    .catch((err) => {
+        console.log("Failed to load KPI data", err);
     });
